9e7a1d623589c4c0727055cd618b741107f18f95
Niels Weistra <n.weistra@itlusions.nl>
2024-11-05 19:36:09 +0100

    Update

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     prefix: str = '/tkn'
-    jobs_namespace: str = 'tkn-bootstrap'
+    jobs_namespace = 'tkn-bootstrap'
 
     class Config:
         env_file = '.env'
