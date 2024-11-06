34c46e7bec9da4bb13dbee7f4293c362a2b0cc71
Niels Weistra <n.weistra@itlusions.nl>
2024-11-07 00:04:26 +0100

    Update

--- src/base/logging.py
+++ src/base/logging.py
@@ -2,15 +2,13 @@ import sys
 import logging.config
 from uvicorn.logging import AccessFormatter
 
-access_log_format = '%(asctime)s :: %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
-
 def get_log_config():
     return {
         'version': 1,
         'disable_existing_loggers': False,
         'formatters': {
             'default': {'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'},
-            'access': {'format': '%(asctime)s :: %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'},
+            'access': {'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'},
         },
         'handlers': {
             'default': {'formatter': 'default', 'class': 'logging.StreamHandler', 'stream': sys.stderr},
@@ -23,6 +21,5 @@ def get_log_config():
         'root': {'level': 'DEBUG', 'handlers': ['default'], 'propagate': False},
     }
 
-# Apply logging configuration directly here
 def setup_logging():
     logging.config.dictConfig(get_log_config())
