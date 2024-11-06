d20713bf5461b8c68117123d6793d5d202a81030
Niels Weistra <n.weistra@itlusions.nl>
2024-11-06 23:28:11 +0100

    Add logging field

--- src/middleware/log_client_ip.py
+++ src/middleware/log_client_ip.py
@@ -7,6 +7,8 @@ logger = logging.getLogger("uvicorn.access")
 class ClientIPLoggerMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         client_ip = request.client.host
+        logger = logging.getLogger("uvicorn.access")
+        logger = logging.LoggerAdapter(logger, {"client_addr": client_ip})
         logger.info(f"Client IP: {client_ip}")
         response = await call_next(request)
         return response
