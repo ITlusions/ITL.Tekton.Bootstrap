742230aadcecc32948e84f14852f9bd4f9444778
Niels Weistra <n.weistra@itlusions.nl>
2024-11-06 23:50:45 +0100

    Update logging

--- src/middleware/log_client_ip.py
+++ src/middleware/log_client_ip.py
@@ -6,7 +6,9 @@ logger = logging.getLogger("uvicorn.access")
 
 class ClientIPLoggerMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        client_ip = request.client.host
+        client_ip = request.headers.get("X-Forwarded-For")
+        if not client_ip:
+            client_ip = request.client.host
         logger = logging.getLogger("uvicorn.access")
         logger = logging.LoggerAdapter(logger, {"client_addr": client_ip})
         logger.info(f"Client IP: {client_ip}")
