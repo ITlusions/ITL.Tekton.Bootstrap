fc76346a6dcc39c1265cc000a25044118cf7e8e2
Niels Weistra <n.weistra@itlusions.nl>
2024-11-06 01:37:24 +0100

    Update logging

--- src/controllers/job_controller.py
+++ src/controllers/job_controller.py
@@ -2,30 +2,30 @@ from kubernetes import client, config
 from models.job_model import JobSpec
 from config.settings import settings 
 
-# # Load the Kubernetes configuration
-# config.load_incluster_config()
+# Load the Kubernetes configuration
+config.load_incluster_config()
 
-# def create_job(job_spec: JobSpec):
-#     job = client.V1Job(
-#         api_version="batch/v1",
-#         kind="Job",
-#         metadata=client.V1ObjectMeta(name=job_spec.name),
-#         spec=client.V1JobSpec(
-#             template=client.V1PodTemplateSpec(
-#                 metadata=client.V1ObjectMeta(labels={"job": job_spec.name}),
-#                 spec=client.V1PodSpec(
-#                     containers=[
-#                         client.V1Container(
-#                             name="alpine-container",
-#                             image="alpine:latest",
-#                             command=["/bin/sh", "-c", "echo $REPO_URL"],
-#                             env=[client.V1EnvVar(name="REPO_URL", value=job_spec.repo_url)]
-#                         )
-#                     ],
-#                     restart_policy="Never"
-#                 )
-#             )
-#         )
-#     )
-#     batch_v1 = client.BatchV1Api()
-#     batch_v1.create_namespaced_job(namespace=settings.jobs_namespace, body=job)
+def create_job(job_spec: JobSpec):
+    job = client.V1Job(
+        api_version="batch/v1",
+        kind="Job",
+        metadata=client.V1ObjectMeta(name=job_spec.name),
+        spec=client.V1JobSpec(
+            template=client.V1PodTemplateSpec(
+                metadata=client.V1ObjectMeta(labels={"job": job_spec.name}),
+                spec=client.V1PodSpec(
+                    containers=[
+                        client.V1Container(
+                            name="alpine-container",
+                            image="alpine:latest",
+                            command=["/bin/sh", "-c", "echo $REPO_URL"],
+                            env=[client.V1EnvVar(name="REPO_URL", value=job_spec.repo_url)]
+                        )
+                    ],
+                    restart_policy="Never"
+                )
+            )
+        )
+    )
+    batch_v1 = client.BatchV1Api()
+    batch_v1.create_namespaced_job(namespace=settings.jobs_namespace, body=job)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers.job_router import router as job_router
 from config.settings import settings
+import sys
 
 app = FastAPI(docs_url=f"{settings.prefix}/docs", openapi_url=f"{settings.prefix}/openapi.json")
 
@@ -8,7 +9,45 @@ app.include_router(job_router)
 
 if __name__ == "__main__":
     import uvicorn
-    log_config = uvicorn.config.LOGGING_CONFIG
-    log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
-    log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+    log_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'default': {
+                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            },
+            'access': {
+                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            },
+        },
+        'handlers': {
+            'default': {
+                'formatter': 'default',
+                'class': 'logging.StreamHandler',
+                'stream': sys.stderr,
+            },
+            'access': {
+                'formatter': 'access',
+                'class': 'logging.StreamHandler',
+                'stream': sys.stdout,
+            },
+        },
+        'loggers': {
+            'uvicorn.error': {
+                'level': 'INFO',
+                'handlers': ['default'],
+                'propagate': False,
+            },
+            'uvicorn.access': {
+                'level': 'INFO',
+                'handlers': ['access'],
+                'propagate': False,
+            },
+        },
+        'root': {
+            'level': 'DEBUG',
+            'handlers': ['default'],
+            'propagate': False,
+        },
+    }
     uvicorn.run(app, host="0.0.0.0", port=8000, proxy_headers=True, log_config=log_config, forwarded_allow_ips='*')
